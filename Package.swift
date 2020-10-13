7bbc7643602b3ad8f919fa2dc1d30522c3340b5d
Mark Webb <Testablekarma@users.noreply.github.com>
2020-10-13 13:42:50 +0100

    Removes ios-core-lib classes

--- Package.swift
+++ Package.swift
@@ -14,14 +14,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/hmrc/ios-core-library.git", from: "2.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ios-test-helpers",
-            dependencies: ["ios-core-library"]),
+            dependencies: []),
         .testTarget(
             name: "ios-test-helpersTests",
             dependencies: ["ios-test-helpers"]),
