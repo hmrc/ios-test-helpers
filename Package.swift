6e94efc4d1a2f694fc251e7a2b2a88e79405d23b
Mark Webb <Testablekarma@users.noreply.github.com>
2020-10-14 15:35:27 +0100

    Help

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ios-test-helpers",
-            targets: ["ios-test-helpers"]),
+            targets: ["ios-test-helpers"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "ios-test-helpersTests",
-            dependencies: ["ios-test-helpers"]),
+            dependencies: ["ios-test-helpers"])
     ]
 )
--- Tests/ios-test-helpersTests/XCTestManifests.swift
+++ Tests/ios-test-helpersTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(ios_test_helpersTests.allTests),
+        testCase(ios_test_helpersTests.allTests)
     ]
 }
 #endif
